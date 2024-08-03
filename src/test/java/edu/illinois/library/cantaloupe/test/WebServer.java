5c0374a3c9a6c6d0f3597c249517cac78829fc5f
Benjamin Prud'homme <139664105+bgprudhomme@users.noreply.github.com>
2024-08-03 19:20:20 -0400

    Update WebServer.java

--- src/test/java/edu/illinois/library/cantaloupe/test/WebServer.java
+++ src/test/java/edu/illinois/library/cantaloupe/test/WebServer.java
@@ -2,7 +2,6 @@ package edu.illinois.library.cantaloupe.test;
 
 import edu.illinois.library.cantaloupe.http.Server;
 import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 
