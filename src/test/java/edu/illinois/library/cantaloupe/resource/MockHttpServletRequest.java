dd2a82d3bf87c1e81d8504209f0adae2c924ebc5
Benjamin Prud'homme <139664105+bgprudhomme@users.noreply.github.com>
2024-08-03 16:20:58 -0400

    Update MockHttpServletRequest.java

--- src/test/java/edu/illinois/library/cantaloupe/resource/MockHttpServletRequest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resource/MockHttpServletRequest.java
@@ -38,25 +38,25 @@ public class MockHttpServletRequest implements HttpServletRequest {
         return null;
     }
 
-    // @Override
-    // public String getRequestId() {
-        // return null;
-    // }
+    @Override
+    public String getRequestId() {
+        return null;
+    }
 
-    // @Override
-    // public boolean authenticate(HttpServletResponse response) {
-        // return false;
-    // }
+    @Override
+    public boolean authenticate(HttpServletResponse response) {
+        return false;
+    }
 
-    // @Override
-    // public String changeSessionId() {
-        // return null;
-    // }
+    @Override
+    public String changeSessionId() {
+        return null;
+    }
 
-    // @Override
-    // public AsyncContext getAsyncContext() {
-        // return null;
-    // }
+    @Override
+    public AsyncContext getAsyncContext() {
+        return null;
+    }
 
     @Override
     public Object getAttribute(String name) {
