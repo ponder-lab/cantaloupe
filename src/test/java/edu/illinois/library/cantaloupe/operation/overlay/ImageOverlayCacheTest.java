b9ca8d2c04d871a828c372d483c8870e84b9d826
Benjamin Prud'homme <bprudhomme@gradcenter.cuny.edu>
2024-08-12 10:25:06 -0400

    One more addition

--- src/test/java/edu/illinois/library/cantaloupe/operation/overlay/ImageOverlayCacheTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/operation/overlay/ImageOverlayCacheTest.java
@@ -15,23 +15,52 @@ import java.util.concurrent.Callable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import static edu.illinois.library.cantaloupe.test.PerformanceTestConstants.*;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = WARMUP_ITERATIONS,
+        time = WARMUP_TIME)
+@Measurement(iterations = MEASUREMENT_ITERATIONS,
+        time = MEASUREMENT_TIME)
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgs = { "-server", "-Xms128M", "-Xmx128M", "-Dcantaloupe.config=memory" })
 public class ImageOverlayCacheTest extends BaseTest {
 
     private static WebServer webServer;
     private ImageOverlayCache instance;
+    
+    @Param({"10", "50", "100", "500", "1000", "5000"})
+    private int numThreads;
 
     @BeforeAll
+    @Setup
     public static void beforeClass() throws Exception {
         webServer = new WebServer();
         webServer.start();
     }
 
     @AfterAll
+    @TearDown
     public static void afterClass() throws Exception {
         webServer.stop();
     }
 
     @BeforeEach
+    @Setup
     public void setUp() throws Exception {
         super.setUp();
         instance = new ImageOverlayCache();
@@ -66,14 +95,15 @@ public class ImageOverlayCacheTest extends BaseTest {
     }
 
     @Test
-    void testPutAndGetConcurrently() throws Exception {
+    @Benchmark
+    public void testPutAndGetConcurrently() throws Exception {
         Callable<Void> callable = () -> {
             URI uri = new URI(webServer.getHTTPURI() + "/jpg");
             byte[] bytes = instance.putAndGet(uri);
             assertEquals(1584, bytes.length);
             return null;
         };
-        new ConcurrentReaderWriter(callable, callable, 5000).run();
+        new ConcurrentReaderWriter(callable, callable, numThreads).run();
     }
 
 }
